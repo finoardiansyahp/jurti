ede95a6ac64c32b1f08e025f73ae9e51d38dc8c9
Unknown <finoardiansyah38@gmail.com>
2017-10-08 10:33:16 +0700

    Auto stash before merge of "master" and "origin/master"

--- database/seeds/BeasiswaSeeder.php
+++ database/seeds/BeasiswaSeeder.php
@@ -13,42 +13,16 @@ class BeasiswaSeeder extends Seeder
     public function run()
     {
         Beasiswa::create([
-<<<<<<<
             'id' => '100',
-=======
-            'id' => '1',
->>>>>>>
             'judul' => 'Beasiswa Bank Indonesia',
-<<<<<<<
             'keterangan' => 'GenBI - Beasiswa BI 2017/2018 khusus Beasiswa Bank Indonesia 2017 Beasiswa menjadi suatu hal yang paling   diinginkan pelajar, tidak terkecuali mahasiswa. Beasiswa merupakan sebuah bantuan yang diberikan oleh lembaga, instansi atau perorangan dengan cara memperingan biaya pendidikan seseorang. Pintar dan berprestasi menjadi syarat mutlak untuk mendapatkan beasiswa. Namun beasiswa sendiri dapat dibedakan menjadi beberapa jenis. Ada beasiswa yang diberikan kepada mahasiswa yang berprestasi dan berasal dari keluarga yang kurang berada, dan juga beasiswa yang diberikan kepada semua mahasiswa yang berprestasi baik dari keluarga kaya maupun miskin. Salah satu jenis beasiswa yang dapat diperoleh oleh mahasiswa strata satu yaitu beasiswa Bank Indonesia. Hampir setiap tahun Bank Indonesia mengeluarkan beasiswa dengan syarat dan ketentuan untuk penerima beasiswa. 
             Informasi beasiswa Bank Indonesia dan Tujuannya
 Hampir serupa dengan jenis beasiswa lainnya, beasiswa Bank Indonesia tentu memiliki tujuan dengan adanya beasiswa ini. Tujuan keberadaan beasiswa ini yaitu untuk meningkatkan kualitas generasi muda terutama bagi para mahasiswa. Beasiswa ini juga menjadi bagian kegiatan sosial yang dilakukan oleh Bank Indonesia. Kisaran beasiswa yang dikeluarkan juga bernilai cukup bervariasi dan terbilang cukup menggiurkan, yaitu sekitar Rp 500.000,- hingga Rp 750.000,- untuk setiap bulan. Beasiswa ini juga termasuk ke dalam jenis beasiswa yang dikeluarkan untuk keluarga dari kalangan tidak mampu yang berprestasi.
 ',
         ]);
-=======
-            'keterangan' => 'GenBI - Beasiswa BI 2017/2018 khusus Beasiswa Bank Indonesia 2017 Beasiswa menjadi suatu hal yang paling diinginkan pelajar, tidak terkecuali mahasiswa. Beasiswa merupakan sebuah bantuan yang diberikan oleh lembaga, instansi atau perorangan dengan cara memperingan biaya pendidikan seseorang. Pintar dan berprestasi menjadi syarat mutlak untuk mendapatkan beasiswa. Namun beasiswa sendiri dapat dibedakan menjadi beberapa jenis.
-            Ada beasiswa yang diberikan kepada mahasiswa yang berprestasi dan berasal dari keluarga yang kurang berada, dan juga beasiswa yang diberikan kepada semua mahasiswa yang berprestasi baik dari keluarga kaya maupun miskin. Salah satu jenis beasiswa yang dapat diperoleh oleh mahasiswa strata satu yaitu beasiswa Bank Indonesia. Hampir setiap tahun Bank Indonesia mengeluarkan beasiswa dengan syarat dan ketentuan untuk penerima beasiswa.
-            Ketentuan beasiswa Bank Indonesia
-            Selain persyaratan yang ditujukan untuk penerima beasiswa Bank Indonesia, beasiswa dari Bank Indonesia ini juga memberikan ketentuan beasiswa sebagai berikut, Untuk dapat mengikuti beasiswa dari Bank Indonesia tahun 2017-2018 harus berasal dari fakultas yang telah ditentukan oleh Bank Indonesia. Mahasiswa dari fakultas sebagai berikut yang dapat mengikuti beasiswa dari Bank Indonesia. Fakultas tersebut diantarnya yaitu;
-            •	Fakultas Hukum
-            •	Fakultas Ekonomi yang meliputi berbagai jurusan seperti Manajemen dan Akuntansi, dan jurusan Ilmu Ekonomi Studi Pembangunan.
-            •	Fakultas Kesehatan Masyarakat
-            •	Fakultas Ilmu Sosial Politik dengan jurusan Ilmu Komunikasi
-            •	Fakultas Pertanian atau Agrobisnis atau Fakultas Peternakan
-            Dengan jumlahnya yang cukup besar, menerima beasiswa dari Bank Indonesia akan sangat membantu untuk mengurangi beban biaya kuliah. Terutama bagi mereka mahasiswa yang berprestasi. Banyak manfaat yang dapat diperoleh dengan menerima beasiswa dari Bank Indonesia. Tidak hanya berupa bantuan biaya pendidikan yang didapat setiap bulannya, banyak manfaat lainnya yang dapat diperoleh dengan menjadi penerima beasiswa dari Bank Indonesia.
-            ',
->>>>>>>
-
-<<<<<<<
-        Beasiswa::create([
-            'id' => '101',
-            'judul' => 'Beasiswa Bank Mandiri',
-            'keterangan' => 'Beasiswa Mandiri - Info Beasiswa Mandiri 2017/2018 Mengenai Beasiswa Mandiri dan Cara Mengikutinya Apakah ada dari Anda yang sedang sibuk mempersiapkan pendidikan lanjut setelah lulus SMA? Dari waktu ke waktu, standarisasi pendidikan di Indonesia kian meningkat. Membuat sebagian besar orang terus berupaya dapat menempuh pendidikan setinggi mungkin. Sama halnya dengan para pelajar yang sebentar lagi lulus. 
-=======
-        ]);
         
         Beasiswa::create([
-            'id' => '2',
+            'id' => '101',
             'judul' => 'Beasiswa Bank Mandiri',
             'keterangan' =>'Beasiswa Mandiri - Info Beasiswa Mandiri 2017/2018 Mengenai Beasiswa Mandiri dan Cara Mengikutinya Apakah ada dari Anda yang sedang sibuk mempersiapkan pendidikan lanjut setelah lulus SMA? Dari waktu ke waktu, standarisasi pendidikan di Indonesia kian meningkat. Membuat sebagian besar orang terus berupaya dapat menempuh pendidikan setinggi mungkin. Sama halnya dengan para pelajar yang sebentar lagi lulus. 
             Tidak sedikit yang berpikir untuk langsung bekerja, namun tidak sedikit juga yang berpikir untuk melanjutkan pendidikan kuliah. Bagi yang berpikir dan berkeinginan ingin melanjutkan pendidikan kuliah, biaya kuliah yang cukup tinggi sering menjadi salah satu kendala. Maka program beasiswa seperti beasiswa Mandiri banyak dicari. Selain untuk meringankan pengeluaran biaya, ada banyak kelebihan yang bisa didapatkan ketika Anda mendapatkan beasiswa tersebut.
@@ -57,39 +31,6 @@ Hampir serupa dengan jenis beasiswa lainnya, beasiswa Bank Indonesia tentu memil
             Indonesia merupakan negara berkembang yang masih terus berproses menjadi lebih baik. Tingkat kemiskinan penduduk Indonesia cukup mengkhawatirkan dan hal ini semakin memperkecil penduduk Indonesia untuk menempuh pendidikan yang tinggi. Bahkan masih banyak sekali yang belum duduk di bangku sekolah. Karena itu lah perlu banyak bantuan dari pihak-pihak yang memang menyadari seberapa daruratnya tingkat pendidikan Indonesia. 
             Bank mandiri dengan beasiswa Mandiri menjadi salah satu pihak yang memiliki kesadaran akan masalah tersebut. Sebab semakin rendahnya tingkat pendidikan penduduk di sebuah negara, semakin rendah pula tingkat kesejahteraannya. Dari segi pemerataan pun, pendidikan di Indonesia belum masuk dalam tingkat yang baik. Program beasiswa membantu untuk meratakannya dan memberi peluang bagi mereka yang memang memiliki kesadaran untuk menempuh pendidikan lebih tinggi, namun memiliki kendala khususnya di masalah pembiayaan.
             ', 
-        ]);
-        
-        Beasiswa::create([
->>>>>>>
-
-<<<<<<<
-Tidak sedikit yang berpikir untuk langsung bekerja, namun tidak sedikit juga yang berpikir untuk melanjutkan pendidikan kuliah. Bagi yang berpikir dan berkeinginan ingin melanjutkan pendidikan kuliah, biaya kuliah yang cukup tinggi sering menjadi salah satu kendala. Maka program beasiswa seperti beasiswa Mandiri banyak dicari. Selain untuk meringankan pengeluaran biaya, ada banyak kelebihan yang bisa didapatkan ketika Anda mendapatkan beasiswa tersebut.
-Jenis beasiswa serta lembaga atau organisasi yang menaunginya bermacam-macam. Mulai dari beasiswa resmi dari pemerintah, hingga perusahaan-perusahaan. Termasuk salah satunya Bank Mandiri yang rutin sejak tahun 2007 menawarkan program beasiswa. Tentu dengan sejumlah tujuan positif. Bagi Anda yang berminat mengikuti program beasiswa dari Bank Mandiri,  berikut ulasannya sebagai penambah informasi seputar beasiswa Mandiri yang setiap tahun diadakan. Program ini bekerjasama dengan berbagai instansi dan universitas. Ini juga dapat menjadi salah satu solusi untuk mendapat pendidikan lanjut dengan biaya yang lebih ringan atau bahkan gratis.
-Tujuan dan Latar Belakang Program Beasiswa Bank Mandiri
-
-Indonesia merupakan negara berkembang yang masih terus berproses menjadi lebih baik. Tingkat kemiskinan penduduk Indonesia cukup mengkhawatirkan dan hal ini semakin memperkecil penduduk Indonesia untuk menempuh pendidikan yang tinggi. Bahkan masih banyak sekali yang belum duduk di bangku sekolah. Karena itu lah perlu banyak bantuan dari pihak-pihak yang memang menyadari seberapa daruratnya tingkat pendidikan Indonesia. 
-',
-=======
-            'id' => '3',
-            'judul' => 'Beasiswa LPDP',
-            'keterangan' =>'Akhirnya kabar yang ditunggu ini muncul juga. Beasiswa LPDP 2017 kini resmi dibuka. LPDP menyediakanbeasiswa S2, beasiswa S3 dan sejumlah program beasiswa menarik lainnya. Salah satu beasiswa LPDP yang paling diminati adalah Beasiswa Pendidikan Indonesia(BPI) Program Magister dan Doktoral.
-            Di bawah akan diuraikan bagaimana Anda harus mendaftar beasiswa S2 dan S3 LPDP tersebut. Termasuk dokumen apa saja yang perlu dipersiapkan sebelum mengajukan beasiswa.
-            BPI Program Magister dan Doktoral ditujukan bagi setiap warganegara Indonesia. Beasiswa S2 LPDP bisa dimanfaatkan untuk melanjutkan studi S2 di dalam maupun luar negeri, begitu pun beasiswa S3 LPDP. Yang mungkin membedakan hanya ketentuan dan persyaratan yang harus dipenuhi. Tapi, kabar gembiranya beasiswa LPDP adalah beasiswa penuh. Sehingga semua kebutuhan studi, seperti biaya kuliah, biaya hidup, biaya keberangkatan dan kepulangan akan ditanggung oleh LPDP. Tidak itu saja, biaya seperti tunjangan keluarga, tunjangan buku, tunjangan tesis/disertasi, seminar, visa, hingga asuransi kesehatan juga akan disediakan. Ada pula fasilitas lain semisal biaya pendaftaran ke universitas, dan tunjangan kedatangan. 
-            Beasiswa LPDP sendiri termasuk salah satu beasiswa prestasi. Pernah diulas sebelumnya. Pelamar harus memiliki kemampuan akademik yang unggul serta mempunyai jiwa kepemimpinan yang kuat. Anda bisa mendaftar beasiswa ini untuk program studi yang sama atau berbeda dengan program studi yang sudah diambil sebelumnya. Namun, untuk universitasnya LPDP sudah menetapkan Daftar Universitas Tujuan LPDP. Pelamar bisa memilih universitas yang diminati dari daftar yang telah ditetapkan tersebut. 
-            Bidang Ilmu Prioritas BPI Program Magister dan Doktoral: 
-            1. Bidang Teknik
-            2. Bidang Sains
-            3. Bidang Pertanian
-            4. Bidang Kedokteran dan Kesehatan
-            5. Bidang Akuntansi dan Keuangan
-            6. Bidang Hukum
-            7. Bidang Agama
-            8. Bidang Pendidikan
-            9. Bidang Sosial
-            10. Bidang Ekonomi
-            11. Bidang Budaya, Seni dan Bahasa
-            ',
->>>>>>>
         ]);
 
         Beasiswa::create([
